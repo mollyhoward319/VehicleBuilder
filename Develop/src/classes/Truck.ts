adfada1b3c09caf96c2c0200bc879d15317a813f
[mollyhoward] <mollyhoward319@gmail.com>
2024-11-04 14:47:32 -0600

    fixed bugs in Truck.ts

--- Develop/src/classes/Truck.ts
+++ Develop/src/classes/Truck.ts
@@ -6,83 +6,52 @@ import Wheel from './Wheel.js';
 import AbleToTow from '../interfaces/AbleToTow.js';
 import { Interface } from 'readline';
 
+
 class Truck extends Vehicle implements AbleToTow {
-  vin: string; 
-  color: string; 
-  make: string; 
-  model: string; 
-  year: number; 
-  weight: number; 
-  topSpeed: number; 
-  wheels: Wheel[]; 
-  towingCapacity: number;
-  
-  constructor(
-    vin: string, 
-    color: string, 
-    make: string, 
-    model: string, 
-    year: number, 
-    weight: number, 
-    topSpeed: number, 
-    wheels: Wheel[], 
-    towingCapacity: number
-  ) {
-    super();
-    this.vin = vin;
-    this.color = color;
-    this.make = make;
-    this.model = model;
-    this.year = year;
-    this.weight = weight;
-    this.topSpeed = topSpeed;
-    this.wheels = wheels;
-    this.towingCapacity = towingCapacity;
-    if (wheels.length === 4) {
-      this.wheels = wheels;
-    } else {
-      this.wheels = [];
-      for (let i=0; i < 4, i++){
-      this.wheels.push(new Wheel());
-      }
+    vin: string; color: string; make: string; model: string; year: number; weight: number; topSpeed: number; wheels: Wheel[]; towingCapacity: number;
+
+    constructor(vin: string, color: string, make: string, model: string, year: number, weight: number, topSpeed: number, wheels: Wheel[], towingCapacity: number) {
+        super();
+        this.vin = vin;
+        this.color = color;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.weight = weight;
+        this.topSpeed = topSpeed;
+        this.wheels = wheels;
+        this.towingCapacity = towingCapacity;
+
+        if (wheels.length === 4) {
+            this.wheels = wheels;
+        } else {
+            this.wheels = [];
+            for (let i = 0; i < 4; i++) {
+                this.wheels.push(new Wheel());
+            }
+        }
     }
-  
-  tow(vehicle: Truck | Motorbike | Car): void {
-   
-  const vehicleTest = vehicle.make && vehicle.model ? `make: #{vehicle.make} model:
-    if (vehicle.weight <= this.towingCapacity) {
-      console.log(`Vehicle is being towed.`);
-    } else {
-      console.log(`The vehicle is too heavy to be towed.`);
+    tow(vehicle: Truck | Motorbike | Car): void {
+        console.log('vehicle: ', vehicle);
+        const vehicleTest = vehicle.make && vehicle.model ? `make: ${vehicle.make} model: ${vehicle.model}` : "no vehicle"
+        if (vehicle.weight <= this.towingCapacity) {
+            console.log('The vehicle is being Towed')
+        }
+        else {console.log('This vehicle is too heavy to be towed') }
     }
-  }
-}
-  
-  override printDetails(): void {
-    super.printDetails();
-    console.log(`VIN: ${this.vin}`);
-    console.log(`Make: ${this.make}`);
-    console.log(`Model: ${this.model}`);
-    console.log(`Year: ${this.year}`);
-    console.log(`Weight: ${this.weight}`);
-    console.log(`Top Speed: ${this.topSpeed}`);
-    console.log(`Color: ${this.color}`);
-    console.log(`Towing Capacity: ${this.towingCapacity}`);
- console.log(
-  `Wheel 1: ${this.wheels[0].getDiameter} inch with a ${this.wheels[0].getTireBrand} tire`
- );
-  console.log(
-    `Wheel 2: ${this.wheels[1].getDiameter} inch with a ${this.wheels[1].getTireBrand} tire`
-  );
-  console.log(
-    `Wheel 3: ${this.wheels[2].getDiameter} inch with a ${this.wheels[2].getTireBrand} tire`
-  );
-  console.log(
-    `Wheel 4: ${this.wheels[3].getDiameter} inch with a ${this.wheels[3].getTireBrand} tire`
-  );
-  }
+    override printDetails(): void {
+        super.printDetails();
+        console.log(this.vin);
+        console.log(this.color);
+        console.log(this.make);
+        console.log(this.model);
+        console.log(this.year);
+        console.log(this.weight);
+        console.log(this.topSpeed);
+        console.log(this.wheels);
+        console.log(this.towingCapacity);
+    }
 }
 
 // Export the Truck class as the default export
 export default Truck;
-
